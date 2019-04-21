35d54ed877d6a43feb51156457aa0a36512a8c3d
“yanglongfei” <13261742917@163.com>
2019-04-21 18:51:20 +0800

    base

--- app1/src/app/pages/base/base.component.ts
+++ app1/src/app/pages/base/base.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BaseComponent implements OnInit {
 
+  /*
+ * 声明属性的几种方式
+ * 1、public  公共的 可以在自己的类以及其他的类里面使用
+ *
+ * 2、private 私有的 只有在当前的类里面才可以访问这个属性
+ *
+ * 3、protected 保护的  只有在当前类以及他的子类里面使用
+ *
+ */
+  public title:string = 'angular'
+  public list:Array<any> = [123,'abc',true]
+  public list1:any[] = ['aaa','bbb',111]
+  public flags:boolean = false
+  public status:number = 3 // 1下单 2交易进行中 3交易完成
+  public attr:string = 'pink'
+  public obj:object = {'name':'张三','age':20 }
+  public tpl:any = "<h1>定义HTML模板</h1>"
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  btn(){
+    alert('你点了我一下')
+  }
 }
