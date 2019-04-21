a9555cefb1f622809dca904822cc5115da50e19a
“yanglongfei” <13261742917@163.com>
2019-04-21 19:23:22 +0800

    form

--- app1/src/app/pages/form/form.component.ts
+++ app1/src/app/pages/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  public peopleInfo: any = {
+    username: '',
+    sex: '2',
+    cityList: ['北京', '上海', '深圳'],
+    city: '上海',
+    hobby: [{
+      title: '吃饭',
+      checked: false
+    }, {
+      title: '睡觉',
+      checked: false
+    }, {
+      title: '打豆豆',
+      checked: true
+    }],
+    mark: ''
+  }
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  getData() {
+    alert(JSON.stringify(this.peopleInfo))
+  }
 }
--- app1/src/app/pages/form/form.module.ts
+++ app1/src/app/pages/form/form.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms'; // 双向绑定
 import { FormRoutingModule } from './form-routing.module';
 import { FormComponent } from './form.component';
 
 @NgModule({
-  declarations: [FormComponent],
+  declarations: [ //声明
+    FormComponent
+  ],
   imports: [
     CommonModule,
-    FormRoutingModule
+    FormRoutingModule,
+    FormsModule
   ]
 })
 export class FormModule { }
