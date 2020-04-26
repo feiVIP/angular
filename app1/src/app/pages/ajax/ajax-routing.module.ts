69c73fbc86a91d50984157e5680b1a33e546fa6e
fei <13261742917@163.com>
2020-04-26 19:26:30 +0800

    add ajax fn

--- app1/src/app/pages/ajax/ajax-routing.module.ts
+++ app1/src/app/pages/ajax/ajax-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AjaxComponent } from './ajax.component';
 const routes: Routes = [{
-  path:'',
-  component:AjaxComponent
+  path: '',
+  component: AjaxComponent
 }];
 
 @NgModule({
--- app1/src/app/pages/ajax/ajax.component.ts
+++ app1/src/app/pages/ajax/ajax.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-ajax',
   templateUrl: './ajax.component.html',
   styleUrls: ['./ajax.component.css']
 })
 export class AjaxComponent implements OnInit {
-
-  constructor() { }
+  // tslint:disable-next-line:ban-types
+  listData: any = [];
+  constructor(public http: HttpClient) { }
 
   ngOnInit() {
+    const api = 'http://a.itying.com/api/productlist'
+    // tslint:disable-next-line:variable-name
+    this.http.get(api).subscribe((response: any) => {
+      this.listData = response.result;
+      console.log(response);
+    });
   }
 
 }
